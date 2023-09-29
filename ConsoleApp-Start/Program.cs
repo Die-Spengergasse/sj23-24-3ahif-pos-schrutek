55eba21fa551d57e777fb9f5dca4617a437b8b99
schrutek <martin@schrutek>
2023-09-29 14:10:38 +0200

    Solution von TT stolen
    Issues
    * Properties
    * Value types
    * Reference Types
    
    Signed by SRM (schrutek@spengergasse.at)

---
+++ ConsoleApp-Start/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ConsoleApp_Start;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hallo, Welt!");
+
+Person person = new Person(new DateTime(2000, 9, 22), "Max Muster");
+Console.WriteLine(person);
+
